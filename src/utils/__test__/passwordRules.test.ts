d9d707c240e2dadc78e07ec3692dbcfd30566935
Mika Reich <mikareich0@gmail.com>
2021-03-21 10:29:31 +0100

    write tests for the lowercase password rule

--- src/utils/__test__/passwordRules.test.ts
+++ src/utils/__test__/passwordRules.test.ts
@@ -1,4 +1,4 @@
-import { uppercaseContained } from "../passwordRules";
+import { lowercaseContained, uppercaseContained } from "../passwordRules";
 
 test("Test uppercase rule for the following password: d{G5-SH[(<O", () => {
   const password = "d{G5-SH[(<O";
@@ -12,4 +12,18 @@ test("Test uppercase rule for the following password: d{g5-sh[(<o", () => {
   const containsUppercase = uppercaseContained.test(password);
 
   expect(containsUppercase).toBe(false);
+});
+
+test("Test lowercase rule for the following password: 5m;A'eyS10)QK", () => {
+  const password = "5m;A'eyS10)QK";
+  const containsLowercase = lowercaseContained.test(password);
+
+  expect(containsLowercase).toBe(true);
+});
+
+test("Test lowecase rule for the following password: 5M;A'EYS10)QK", () => {
+  const password = "5M;A'EYS10)QK";
+  const containsLowercase = lowercaseContained.test(password);
+
+  expect(containsLowercase).toBe(false);
 });
